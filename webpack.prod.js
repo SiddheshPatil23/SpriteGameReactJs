2cde7c6bf898c18cc872c9eb49c9ecba8654e0c7
SiddheshPatil23 <siddheshpatil.sp23@gmail.com>
2024-11-04 09:53:09 +0530

    first commit

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,6 @@
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
-const path = require("path"); // Import path module to resolve paths
 
 process.env["NODE_ENV"] = "production";
 
@@ -9,14 +8,11 @@ module.exports = merge([
   common,
   {
     mode: "production",
-    output: {
-      filename: "app.js", 
-      path: path.resolve(__dirname, "build"), 
-    },
     optimization: {
       minimize: true,
       minimizer: [
-      
+        // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
+        // `...`,
         new CssMinimizerPlugin(),
       ],
     },
